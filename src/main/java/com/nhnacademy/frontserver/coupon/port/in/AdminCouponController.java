337e0b685a7f3a5ad4eb31d10ab923a5decf01a9
hyunsu15 <wjdgustn15@naver.com>
2025-12-10 09:52:20 +0900

    feat : 쿠폰 생성 & 쿠폰 조회

--- src/main/java/com/nhnacademy/frontserver/coupon/port/in/AdminCouponController.java
+++ src/main/java/com/nhnacademy/frontserver/coupon/port/in/AdminCouponController.java
@@ -1,23 +1,30 @@
 package com.nhnacademy.frontserver.coupon.port.in;
 
 import com.nhnacademy.frontserver.common.Page;
-import com.nhnacademy.frontserver.coupon.port.out.AdminCouponPolicyClient;
+import com.nhnacademy.frontserver.coupon.port.out.CouponClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/admin/coupon-policies")
+@RequestMapping("/admin/coupons")
 @Slf4j
 public class AdminCouponController {
-    private final AdminCouponPolicyClient adminCouponPolicyClient;
+    private final CouponClient couponClient;
+
+    @PostMapping
+    public String createCoupon(CouponCreateRequest request) {
+        couponClient.createCoupon(request);
+        return "redirect:/admin/coupon-policies";
+    }
     @GetMapping
-    public String getIndexPage(Model model, Page page) {
-        model.addAttribute("contents",adminCouponPolicyClient.getPolices(page.pageNumber(), page.pageSize()));
-        return "adminCouponPolicy";
+    public String getIndexPage(Page page, Model model) {
+        model.addAttribute("contents",couponClient.getCoupons(page.pageNumber(),page.pageSize()));
+        return "admin-coupon-list";
     }
 }
