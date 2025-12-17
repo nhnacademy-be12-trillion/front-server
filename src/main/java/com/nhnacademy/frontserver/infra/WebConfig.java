7ed6236b84819ab340861b0bcac1d475b2bc3353
YI SEUNGHYEON <tmdgus0698@gmail.com>
2025-12-17 10:56:09 +0900

    refactor: /infra 브런치와 병합 완료

--- src/main/java/com/nhnacademy/frontserver/infra/WebConfig.java
+++ src/main/java/com/nhnacademy/frontserver/infra/WebConfig.java
@@ -2,9 +2,10 @@ package com.nhnacademy.frontserver.infra;
 
 
 import com.nhnacademy.frontserver.CheckTimeInterceptor;
-import com.nhnacademy.frontserver.common.cartsummary.CartBadgeInterceptor;
 import com.nhnacademy.frontserver.infra.argumentResolver.CustomArgumentResolver;
 import java.util.List;
+
+import com.nhnacademy.frontserver.layout.interceptor.LayoutDataInterceptor;
 import lombok.RequiredArgsConstructor;
 import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
 import org.springframework.context.annotation.Bean;
@@ -18,7 +19,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     private final List<CustomArgumentResolver> argumentResolvers;
 
-    private final CartBadgeInterceptor cartBadgeInterceptor;
+    private final LayoutDataInterceptor layoutDataInterceptor;
     private final CheckTimeInterceptor checkTimeInterceptor;
 
     @Override
@@ -29,7 +30,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(cartBadgeInterceptor)
+        registry.addInterceptor(layoutDataInterceptor)
                 .addPathPatterns("/**") // 모든 페이지에 적용
                 .excludePathPatterns( //TODO 추후 추가적으로 제외 패턴 추가
                         "/static/**", "/css/**", "/js/**", "/images/**", // 정적 자원 제외
