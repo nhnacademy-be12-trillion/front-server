a52dd0a250ea072037981f1a2bff9a73d87b14ab
chosun-nhn18 <kimshh21@gmail.com>
2025-12-23 17:40:39 +0900

    fix: JWT 재발급 오류 수정

--- src/main/java/com/nhnacademy/frontserver/auth/config/FeignClientConfig.java
+++ src/main/java/com/nhnacademy/frontserver/auth/config/FeignClientConfig.java
@@ -5,11 +5,13 @@ import feign.RequestInterceptor;
 import feign.Retryer;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+@Slf4j
 @Configuration
 public class FeignClientConfig {
 
@@ -20,29 +22,40 @@ public class FeignClientConfig {
                 return;
             }
 
+            boolean headerSet = false;
+
             String newAccessToken = TokenHolder.get();
             if (newAccessToken != null) {
+                log.info("Feign Interceptor: TokenHolder의 새 토큰 적용");
                 template.header("Authorization", "Bearer " + newAccessToken);
                 TokenHolder.clear();
-                return;
+                headerSet = true;
             }
 
             if (template.headers().containsKey("Authorization")) {
-                return;
+                headerSet = true;
             }
 
             ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-
             if (attributes != null) {
                 HttpServletRequest request = attributes.getRequest();
                 Cookie[] cookies = request.getCookies();
 
                 if (cookies != null) {
+                    StringBuilder cookieHeader = new StringBuilder();
+
                     for (Cookie cookie : cookies) {
-                        if ("accessToken".equals(cookie.getName())) {
+                        if (cookieHeader.length() > 0) {
+                            cookieHeader.append("; ");
+                        }
+                        cookieHeader.append(cookie.getName()).append("=").append(cookie.getValue());
+
+                        if (!headerSet && "accessToken".equals(cookie.getName())) {
                             template.header("Authorization", "Bearer " + cookie.getValue());
+                            headerSet = true;
                         }
                     }
+                    template.header("Cookie", cookieHeader.toString());
                 }
             }
         };
@@ -50,6 +63,7 @@ public class FeignClientConfig {
 
     @Bean
     public Retryer retryer() {
+        // 100ms 간격으로 최대 2회 재시도
         return new Retryer.Default(100L, 1000L, 2);
     }
 }
