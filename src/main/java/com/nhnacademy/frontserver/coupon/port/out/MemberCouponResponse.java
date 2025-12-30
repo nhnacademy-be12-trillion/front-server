20bebbdc72f8a17d79cbdd719d2b49d29f16705f
hyunsu15 <wjdgustn15@naver.com>
2025-12-30 13:36:04 +0900

    chore : Coupon MemberCouponResponse 동일하게 변경
    과한것 같음.

--- src/main/java/com/nhnacademy/frontserver/coupon/port/out/MemberCouponResponse.java
+++ src/main/java/com/nhnacademy/frontserver/coupon/port/out/MemberCouponResponse.java
@@ -3,10 +3,6 @@ package com.nhnacademy.frontserver.coupon.port.out;
 import java.time.LocalDateTime;
 
 
-public record MemberCouponResponse(
-        Long id,
-        Long memberId,
-        Long couponId,
-        boolean use,
-        LocalDateTime lastModifiedDate
-) {}
+public record MemberCouponResponse(Long id, String name, Long quantity, LocalDateTime issueStartDate, LocalDateTime issueEndDate, Long minOrderPrice, Long maxDiscountPrice,
+                                   CouponDiscountType couponDiscountType) {
+}
