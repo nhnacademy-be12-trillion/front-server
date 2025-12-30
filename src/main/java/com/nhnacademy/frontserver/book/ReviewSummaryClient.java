3e43a14b7f196d6f7d3d4f028c12e13ac1cabd57
serrot <parksh568923@gmail.com>
2025-12-30 17:13:49 +0900

    feat. 로그 시스템 적용, 로그 관련 의존성 추가, ReviewSummaryClient 경로 설정
    - feign url 경로 로그 설정 반영
    - logback.xml 팀 컨벤션 로그 시스템 적용
    - ReviewSummaryClient에 gatewayURL 적용

--- src/main/java/com/nhnacademy/frontserver/book/ReviewSummaryClient.java
+++ src/main/java/com/nhnacademy/frontserver/book/ReviewSummaryClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
-@FeignClient(name = "search-service")
+@FeignClient(name = "gateway-book", url = "${gateway.url}")
 public interface ReviewSummaryClient {
     @GetMapping("/api/books/{isbn}/review-summary")
     ReviewSummaryResponse getReviewSummary(@PathVariable("isbn") String isbn);
