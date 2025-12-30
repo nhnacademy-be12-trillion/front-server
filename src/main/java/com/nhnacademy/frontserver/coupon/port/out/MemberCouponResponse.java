6cff96e936e6e172f20e91999972b60805dcde23
hyunsu15 <wjdgustn15@naver.com>
2025-12-30 16:39:18 +0900

    fix : 책 멤버쿠폰 couponId 추가

--- src/main/java/com/nhnacademy/frontserver/coupon/port/out/MemberCouponResponse.java
+++ src/main/java/com/nhnacademy/frontserver/coupon/port/out/MemberCouponResponse.java
@@ -3,6 +3,6 @@ package com.nhnacademy.frontserver.coupon.port.out;
 import java.time.LocalDateTime;
 
 
-public record MemberCouponResponse(Long id, String name, Long quantity, LocalDateTime issueStartDate, LocalDateTime issueEndDate,Double discountValue, Long minOrderPrice, Long maxDiscountPrice,
+public record MemberCouponResponse(Long id, Long couponId,String name, Long quantity, LocalDateTime issueStartDate, LocalDateTime issueEndDate,Double discountValue, Long minOrderPrice, Long maxDiscountPrice,
                                    String couponDiscountType) {
 }
